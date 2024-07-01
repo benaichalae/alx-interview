6ebbfc851d491b24d5c7cd332822c135468e3ef1
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2024-07-01 19:13:10 +0000

    js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+const fetchCharacterName = async (characterUrl) => {
+  try {
+    const response = await axios.get(characterUrl);
+    console.log(response.data.name);
+  } catch (error) {
+    console.error(`Error fetching character data: ${error.message}`);
+  }
+};
+
+const fetchFilmCharacters = async () => {
+  try {
+    const response = await axios.get(url);
+    const characterUrls = response.data.characters;
+
+    for (const characterUrl of characterUrls) {
+      await fetchCharacterName(characterUrl);
+    }
+  } catch (error) {
+    console.error(`Error fetching film data: ${error.message}`);
+  }
+};
+
+fetchFilmCharacters();
